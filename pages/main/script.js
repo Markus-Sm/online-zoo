f35d917c7c3e644502d5926b99d8d0b08f2fa226
Marek Samofał <96687413+Markus-Sm@users.noreply.github.com>
2022-11-30 21:28:38 +0100

    docs: update

--- pages/main/script.js
+++ pages/main/script.js
@@ -1,1 +1,22 @@
 'use strict';
+
+const modalWindow = document.querySelector('.modal__window');
+const closeModalWindow = document.querySelector('.close__modal__window')
+const burger = document.querySelector('.burger')
+
+const openModal = function(){
+    
+    
+    
+    modalWindow.classList.remove('hidden')
+    
+}
+
+const closeModal = function(){
+    modalWindow.classList.add('hidden')
+}
+
+burger.addEventListener('click', openModal);
+closeModalWindow.addEventListener('click', closeModal)
+
+
