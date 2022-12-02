b881aaa592cd20c78d51b2cbaef77231192f67aa
Marek Samofał <96687413+Markus-Sm@users.noreply.github.com>
2022-12-02 11:40:01 +0100

    docs: update

--- pages/main/script.js
+++ pages/main/script.js
@@ -1,1 +1,41 @@
 'use strict';
+
+const modalWindow = document.querySelector('.modal__window');
+const closeModalWindow = document.querySelector('.close__modal__window')
+const burger = document.querySelector('.burger')
+
+const openModal = function(){
+    modalWindow.classList.remove('hidden')
+}
+
+const closeModal = function(){
+    modalWindow.classList.add('hidden')
+}
+
+burger.addEventListener('click', openModal);
+closeModalWindow.addEventListener('click', closeModal)
+
+/* Testimonials */
+
+var sliderMain = document.getElementById('slider-main');
+var item = sliderMain.getElementsByClassName('pet__card');
+
+var sliderMain_2 = document.getElementById('slider-main-2');
+var item_2 = sliderMain_2.getElementsByClassName('pet__card-2');
+
+let arrowLeft = document.querySelector('.left');
+let arrowRight = document.querySelector('.right');
+
+function next(){
+    sliderMain.append(item[0]);
+    sliderMain_2.append(item_2[0]);
+}
+
+function prev(){
+    sliderMain.prepend(item[item.length-1]);
+    sliderMain_2.prepend(item_2[item_2.length-1]);
+}
+
+arrowLeft.addEventListener("click", prev)
+arrowRight.addEventListener("click", next)
+
