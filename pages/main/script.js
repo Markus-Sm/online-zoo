67d82a2d7112348e6ac3fb27539b528cd74cf02a
Marek Samofał <96687413+Markus-Sm@users.noreply.github.com>
2022-12-04 15:51:55 +0100

    docs: update

--- pages/main/script.js
+++ pages/main/script.js
@@ -17,7 +17,9 @@ const closeModal = function(){
 burger.addEventListener('click', openModal);
 closeModalWindow.addEventListener('click', closeModal)
 
+/* ========================= */
 /* pets carousel */
+/* ========================= */
 
 var sliderMain = document.getElementById('slider-main');
 var item = sliderMain.getElementsByClassName('pet__card');
@@ -41,7 +43,9 @@ function prev(){
 arrowLeft.addEventListener("click", prev)
 arrowRight.addEventListener("click", next)
 
+/* ========================= */
 /* testimonials range slider */
+/* ========================= */
 
 const sliderRange = document.querySelector('.slider-progress');
 
@@ -50,7 +54,91 @@ const cards__item = cards__wrapper.getElementsByClassName('square__testimonials'
 
 sliderRange.oninput = function(){
 
-    for(let i = 0; i<= this.value; i++){
-        cards__wrapper.append(cards__item[0]);
+    // for(let i = 0; i <= this.value; i++){
+    //     cards__wrapper.append(cards__item[0]);
+    // }
+
+    if(this.value == 0){
+        cards__wrapper.append(cards__item[0])
+    }else if(this.value == 1){
+        cards__wrapper.append(cards__item[0])
+    }else if(this.value == 2){
+        cards__wrapper.append(cards__item[0])
+    }else if(this.value == 3){
+        cards__wrapper.append(cards__item[0])
+    }else if(this.value == 4){
+        cards__wrapper.append(cards__item[0])
+    }else if(this.value == 5){
+        cards__wrapper.append(cards__item[0])
+    }else if(this.value == 6){
+        cards__wrapper.append(cards__item[0])
+    }else if(this.value == 7){
+        cards__wrapper.append(cards__item[0])
     }
 }
+
+/* ========================= */
+/* Popup Testimonials*/
+/* ========================= */
+
+const catalog_testimonial_cards = document.getElementById('catalog_testimonials');
+
+const markup = testimonials.reduce((acc, value) => {
+
+    return `
+    <div class="square__testimonials id=testimonials-card-id-${value.id}">
+        <div class="testimonials__header">
+            <div class="header__left"><img src="${value.img}" alt="user img"></div>
+                <div class="header__right">
+                <h4 class="header-6">${value.author}</h4>
+                <p>${value.location} › ${value.day}</p>
+            </div>
+        </div>
+
+        <div class="testimonials__text">
+            <p>${value.commend}</p>
+        </div>
+        <div class="modal hidden">
+                    <img src="../../assets/icons/x_modal.png" alt="x icon" class="modal-x">
+                    <div class="modals__card">
+                        <div class="testimonials__header">
+                            <div class="header__left"><img src="${value.img}" alt="user img"></div>
+                            <div class="header__right">
+                                <h4 class="header-6">${value.author}</h4>
+                                <p>${value.location} › ${value.day}</p>
+                            </div>
+                        </div>
+            
+                        <div class="testimonials__text">
+                            <p>${value.commend}</p>
+                        </div>
+                    </div>
+                </div>
+    </div>
+    `.concat(acc);
+}, '');
+
+catalog_testimonial_cards.innerHTML = markup;
+
+const card_testimonials = document.querySelectorAll(".square__testimonials");
+const modal_card = document.querySelectorAll('.modal')
+const overlay_1 = document.querySelector('.overlay1');
+const close__modal = document.querySelectorAll('.modal-x')
+
+function openModalTestimonial(e){
+    e.target.nextElementSibling.nextElementSibling.classList.remove('hidden')
+    overlay_1.classList.remove('hidden')
+}
+
+function closeModalTestimonial(e){    
+    e.target.parentElement.classList.add('hidden');
+    overlay_1.classList.add('hidden');
+}
+
+card_testimonials.forEach(card => {
+    card.addEventListener('click', openModalTestimonial);
+})
+
+close__modal.forEach(x => {
+    x.addEventListener('click', closeModalTestimonial)
+})
